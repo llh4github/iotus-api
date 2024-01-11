e995cffd91799558dc921962deb26e8ba7ce0135
llh4github <61351434+llh4github@users.noreply.github.com>
2024-01-11 16:36:43 +0800

    build: security

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -20,6 +20,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    testImplementation("org.springframework.security:spring-security-test")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
