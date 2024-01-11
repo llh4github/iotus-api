bbf94c0fbd935f2afee6c53350cdab58e0bf5a00
llh4github <61351434+llh4github@users.noreply.github.com>
2024-01-11 17:53:47 +0800

    build: jimmer

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.util.archivesName
 plugins {
     id("kotlin-conventions")
     id("spring-conventions")
+    alias(libs.plugins.ksp)
 }
 
 graalvmNative {
@@ -16,6 +17,7 @@ tasks.withType<Jar> {
     archivesName = rootProject.name
 }
 
+val jimmerVersion = libs.versions.jimmer.get()
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
@@ -27,6 +29,13 @@ dependencies {
 
     implementation(libs.knife4j.openapi3)
     implementation(project(":commons"))
+    runtimeOnly("com.mysql:mysql-connector-j")
+    implementation("org.babyfish.jimmer:jimmer-spring-boot-starter:${jimmerVersion}")
+    ksp("org.babyfish.jimmer:jimmer-ksp:${jimmerVersion}")
 }
-
+kotlin {
+    sourceSets.main {
+        kotlin.srcDir("build/generated/ksp/main/kotlin")
+    }
+}
 
