19138ae996f95463b813dd9916ff6688c6522a9a
llh4github <61351434+llh4github@users.noreply.github.com>
2024-01-18 10:08:32 +0800

    fix: 去掉不合适的类

--- api/src/main/kotlin/io/github/llh4github/lotus/api/api/auth/UserApi.kt
+++ api/src/main/kotlin/io/github/llh4github/lotus/api/api/auth/UserApi.kt
@@ -2,13 +2,7 @@ package io.github.llh4github.lotus.api.api.auth
 
 import io.github.llh4github.lotus.api.api.BaseApi
 import io.github.llh4github.lotus.api.service.auth.UserService
-import io.github.llh4github.lotus.commons.JsonWrapper
-import io.github.llh4github.lotus.model.auth.dto.CreateUserInput
-import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
-import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -25,12 +19,5 @@ class UserApi(
     private val userService: UserService
 ) : BaseApi() {
 
-    @PostMapping
-    @Operation(summary = "添加用户")
-    fun add(
-        @RequestBody @Validated dto: CreateUserInput
-    ): JsonWrapper<Nothing> {
-        userService.add(dto)
-        return ok(null)
-    }
+
 }
--- api/src/main/kotlin/io/github/llh4github/lotus/api/service/auth/UserService.kt
+++ api/src/main/kotlin/io/github/llh4github/lotus/api/service/auth/UserService.kt
@@ -1,12 +1,7 @@
 package io.github.llh4github.lotus.api.service.auth
 
-import io.github.llh4github.lotus.model.PageResult
+import io.github.llh4github.lotus.api.service.BaseService
 import io.github.llh4github.lotus.model.auth.User
-import io.github.llh4github.lotus.model.auth.dto.CreateUserInput
-import io.github.llh4github.lotus.model.auth.dto.UserSimpleQuerySpec
-import org.babyfish.jimmer.View
-import org.babyfish.jimmer.sql.fetcher.Fetcher
-import kotlin.reflect.KClass
 
 /**
  *
@@ -14,11 +9,7 @@ import kotlin.reflect.KClass
  * Created At 2024/1/16 10:20
  * @author llh
  */
-interface UserService {
+interface UserService : BaseService<User> {
 
-    @Deprecated(message = "")
-    fun add(dto: CreateUserInput)
 
-    fun pageQuery(param: UserSimpleQuerySpec, fetcher: Fetcher<User>? = null): PageResult<User>
-    fun <S : View<User>> pageQuery(staticType: KClass<S>, param: UserSimpleQuerySpec): PageResult<S>
 }
