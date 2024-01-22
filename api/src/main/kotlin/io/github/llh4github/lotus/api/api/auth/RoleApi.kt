970393a8453239d6053537801a09c313fe07a34a
llh4github <61351434+llh4github@users.noreply.github.com>
2024-01-22 15:13:34 +0800

    feat: 基本的CUR

--- api/src/main/kotlin/io/github/llh4github/lotus/api/api/auth/RoleApi.kt
+++ api/src/main/kotlin/io/github/llh4github/lotus/api/api/auth/RoleApi.kt
@@ -5,11 +5,9 @@ import io.github.llh4github.lotus.api.service.auth.RoleService
 import io.github.llh4github.lotus.commons.JsonWrapper
 import io.github.llh4github.lotus.model.PageResult
 import io.github.llh4github.lotus.model.auth.Role
-import io.github.llh4github.lotus.model.auth.dto.RoleAddInput
-import io.github.llh4github.lotus.model.auth.dto.RoleSimpleSpec
+import io.github.llh4github.lotus.model.auth.dto.*
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
-import org.babyfish.jimmer.client.ApiIgnore
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 
@@ -28,15 +26,22 @@ class RoleApi(
 
     @PostMapping
     @Operation(summary = "添加角色")
-    fun add(@RequestBody @Validated dto: RoleAddInput): JsonWrapper<Nothing> {
-        roleService.add(dto)
-        return ok(null)
+    fun add(@RequestBody @Validated dto: RoleAddInput): JsonWrapper<Role> {
+        val rs = roleService.add(dto)
+        return ok(rs)
+    }
+
+    @PutMapping
+    @Operation(summary = "修改角色")
+    fun update(@RequestBody @Validated dto: RoleUpdateInput): JsonWrapper<Role> {
+        val rs = roleService.update(dto)
+        return ok(rs)
     }
 
     @Operation(summary = "分页查询(简易信息)")
     @PostMapping("page")
-    fun pageQuery(@RequestBody @Validated query: RoleSimpleSpec): JsonWrapper<PageResult<Role>> {
-        val rs = roleService.pageQuery(query, query.page)
+    fun pageQuery(@RequestBody @Validated query: RoleSimpleSpec): JsonWrapper<PageResult<RoleSimpleView>> {
+        val rs = roleService.pageQueryOutType(RoleSimpleView::class, query, query.page)
         return ok(rs)
     }
 
@@ -46,5 +51,13 @@ class RoleApi(
         val rs = roleService.findById(id)
         return ok(rs)
     }
+
+    @Operation(summary = "角色信息（含对应权限）")
+    @GetMapping("withPermission")
+    fun getWithPermission(id: Long): JsonWrapper<RoleWithPermissionView> {
+        val rs = roleService.findById(RoleWithPermissionView::class, id)
+        return ok(rs)
+    }
+
 
 }
