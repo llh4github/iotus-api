a7142e1202470e3a218b4e75d5973fe0307fdb4d
llh <lilinhong_coding@foxmail.com>
2024-05-31 16:39:52 +0800

    🗃️执行与数据库相关的变更:  脚本文件变更

--- resources/tables.sql
+++ resources/tables.sql
@@ -1,10 +1,11 @@
--- public.auth_role definition
+-- public.auth_user definition
 
 -- Drop table
 
--- DROP TABLE public.auth_role;
+-- DROP TABLE public.auth_user;
 
-CREATE TABLE public.auth_role (
+CREATE TABLE public.auth_user
+(
 	id int8 NOT NULL, -- 数据ID
 	username text NOT NULL, -- 用户名
 	password_hash text NOT NULL, -- 密码
@@ -12,20 +13,28 @@ CREATE TABLE public.auth_role (
 	updated_time timestamp(3) DEFAULT CURRENT_TIMESTAMP NULL, -- 数据更新时间
 	updated_by_user_id int8 NULL, -- 数据更新人ID
 	created_by_user_id int8 NULL, -- 数据创建人ID
-	CONSTRAINT auth_role_pkey PRIMARY KEY (id),
-	CONSTRAINT auth_role_username_key UNIQUE (username)
+    CONSTRAINT auth_user_pkey PRIMARY KEY (id),
+    CONSTRAINT auth_user_username_key UNIQUE (username)
 );
-COMMENT ON TABLE public.auth_role IS '用户数据表';
+COMMENT
+ON TABLE public.auth_user IS '用户数据表';
 
 -- Column comments
 
-COMMENT ON COLUMN public.auth_role.id IS '数据ID';
-COMMENT ON COLUMN public.auth_role.username IS '用户名';
-COMMENT ON COLUMN public.auth_role.password_hash IS '密码';
-COMMENT ON COLUMN public.auth_role.created_time IS '数据创建时间';
-COMMENT ON COLUMN public.auth_role.updated_time IS '数据更新时间';
-COMMENT ON COLUMN public.auth_role.updated_by_user_id IS '数据更新人ID';
-COMMENT ON COLUMN public.auth_role.created_by_user_id IS '数据创建人ID';
+COMMENT
+ON COLUMN public.auth_user.id IS '数据ID';
+COMMENT
+ON COLUMN public.auth_user.username IS '用户名';
+COMMENT
+ON COLUMN public.auth_user.password_hash IS '密码';
+COMMENT
+ON COLUMN public.auth_user.created_time IS '数据创建时间';
+COMMENT
+ON COLUMN public.auth_user.updated_time IS '数据更新时间';
+COMMENT
+ON COLUMN public.auth_user.updated_by_user_id IS '数据更新人ID';
+COMMENT
+ON COLUMN public.auth_user.created_by_user_id IS '数据创建人ID';
 
 -- public.auth_page_meta definition
 
